399e9ee518974b7b7ab848146c0be12a1712f1ae
jsantos <jsantosdev1@gmail.com>
2026-02-22 17:36:39 -0300

    feat: implementando gateways do dynamodb, r2 e entrypoints

---
+++ src/main/java/com/jsantos/certseasy/adapters/dtos/RequisicaoCertDTO.java
@@ -0,0 +1,12 @@
+package com.jsantos.certseasy.adapters.dtos;
+
+public record RequisicaoCertDTO(String titulo,
+                                String descricao,
+                                String categoria,
+                                String urlValidacao,
+                                String dataEmissao,
+                                String nomeArquivo,
+                                String tipoConteudo,
+                                String conteudoBase64
+) {
+}
